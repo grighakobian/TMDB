0109fc3b24c4a19985a7de83e4d464b9dd75d2a3
Grigor Hakobyan <grighakobian@gmail.com>
2021-11-07 16:47:43 +0400

    feat: Created view model assembly

---
+++ TMDB/Kernel/DependencyInjection/ViewModelAssembly.swift
@@ -0,0 +1,16 @@
+//
+//  ViewModelAssembly.swift
+//  TMDB
+//
+//  Created by Grigor Hakobyan on 07.11.21.
+//
+
+import Swinject
+
+public final class ViewModelAssembly: Assembly {
+    
+    public func assemble(container: Container) {
+        
+    }
+}
+
