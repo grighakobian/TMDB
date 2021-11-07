817314c09fa04c2004ff5f517d5e13a5c2aa4c27
Grigor Hakobyan <grighakobian@gmail.com>
2021-11-07 16:48:35 +0400

    feat: Created view controller assembly

---
+++ TMDB/Kernel/DependencyInjection/ViewControllerAssembly.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControllerAssembly.swift
+//  TMDB
+//
+//  Created by Grigor Hakobyan on 07.11.21.
+//
+
+import Swinject
+
+public final class ViewControllerAssembly: Assembly {
+    
+    public func assemble(container: Container) {
+        
+    }
+}
