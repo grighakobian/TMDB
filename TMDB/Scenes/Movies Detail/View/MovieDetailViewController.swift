ced350b9c876a597796b3aa1613b5f446cd6b628
Grigor Hakobyan <grighakobian@gmail.com>
2021-11-10 11:07:19 +0400

    refactor: Refactor movies detail scene

--- TMDB/Scenes/Movies Detail/View/MovieDetailViewController.swift
+++ TMDB/Scenes/Movies Detail/View/MovieDetailViewController.swift
@@ -20,9 +20,10 @@ public class MovieDetailViewController: UIViewController {
     private lazy var stackView = makeStackView()
     private lazy var scrollView = makeScrollView()
     
+    private(set) var viewModel: MovieDetailViewModel
+    private(set) var dataSource: MoviesDataSource!
+    
     private let disposeBag = DisposeBag()
-    public let viewModel: MovieDetailViewModel
-    private var dataSource: MoviesDataSource!
     
     public init(viewModel: MovieDetailViewModel) {
         self.viewModel = viewModel
@@ -81,18 +82,21 @@ extension MovieDetailViewController {
         navigationItem.largeTitleDisplayMode = .never
         
         configureScrollView()
+        configureStackView()
     }
     
     private func configureScrollView() {
         view.addSubview(scrollView)
-        
         let safeArea = view.safeAreaLayoutGuide
         scrollView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
         scrollView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
         scrollView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
-        
+    }
+    
+    private func configureStackView() {
         scrollView.addSubview(stackView)
+        
         let layoutGuide = scrollView.contentLayoutGuide
         stackView.topAnchor.constraint(equalTo: layoutGuide.topAnchor).isActive = true
         stackView.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor).isActive = true
@@ -100,7 +104,9 @@ extension MovieDetailViewController {
         stackView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
         stackView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor).isActive = true
         stackView.centerXAnchor.constraint(equalTo: scrollView.frameLayoutGuide.centerXAnchor).isActive = true
-        
+    }
+    
+    private func configureCollectionView() {
         stackView.addArrangedSubview(movieView)
         stackView.addArrangedSubview(overviewLabel)
         movieView.heightAnchor.constraint(equalToConstant: view.bounds.height * 0.5).isActive = true
@@ -118,76 +124,5 @@ extension MovieDetailViewController {
         collectionView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
-    }
-}
-
-
-// MARK:-  View Factory
-
-extension MovieDetailViewController {
-    
-    private func makeStackView()-> UIStackView {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 20.0
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }
-    
-    private func makeMovieView()-> MovieView {
-        let movieView = MovieView()
-        movieView.clipsToBounds = true
-        movieView.translatesAutoresizingMaskIntoConstraints = false
-        return movieView
-    }
-    
-    private func makeOverviewLabel()-> InsetLabel {
-        let overviewLabel = InsetLabel()
-        overviewLabel.numberOfLines = 0
-        overviewLabel.textColor = UIColor.secondaryLabel
-        overviewLabel.font = UIFont.systemFont(ofSize: 32, weight: .regular)
-        overviewLabel.textInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
-        overviewLabel.translatesAutoresizingMaskIntoConstraints = false
-        return overviewLabel
-    }
-    
-    private func makeScrollView()-> UIScrollView {
-        let scrollView = UIScrollView()
-        scrollView.alwaysBounceVertical = true
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        return scrollView
-    }
-    
-    private func makeContainerView()-> UIView {
-        let containerView = UIView()
-        containerView.backgroundColor = .systemBackground
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        containerView.clipsToBounds = true
-        return containerView
-    }
-    
-    private func makeHeadingLabel()-> InsetLabel {
-        let label = InsetLabel()
-        label.text = "You may also like"
-        label.textColor = UIColor.label
-        label.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
-        label.textInsets = UIEdgeInsets(top: 8, left: 20, bottom: 18, right: 20)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }
-    
-    private func makeCollectionView()-> UICollectionView {
-        let layout = MoviesCollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.itemSize = CGSize(width: 250, height: 300)
-        layout.minimumLineSpacing = 20.0
-        layout.minimumInteritemSpacing = 20.0
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.backgroundColor = .quaternarySystemFill
-        collectionView.register(MovieCollectionViewCell.self)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.decelerationRate = .fast
-        collectionView.showsHorizontalScrollIndicator = false
-        return collectionView
     }
 }
